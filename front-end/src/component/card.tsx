d220e12020ec286b36ed18c8ad6d057600e6a0c9
hibamahboob587 <hibamahboob11@gmail.com>
2026-05-20 03:49:24 +0500

    fix broken image fallback on manga cards

--- front-end/src/component/card.tsx
+++ front-end/src/component/card.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../Style/home.css";
 
 interface Props {
@@ -8,9 +9,15 @@ interface Props {
 }
 
 const Card = ({ name, price, imgsrc, category }: Props) => {
+  const [imgFailed, setImgFailed] = useState(false);
+
   return (
     <div className="item">
-      <img src={imgsrc} alt="" />
+      {imgsrc && !imgFailed ? (
+        <img src={imgsrc} alt={name} onError={() => setImgFailed(true)} />
+      ) : (
+        <div className="no-image">NO IMAGE AVAILABLE</div>
+      )}
       <h4>{name}</h4>
       <p>{category}</p>
       <ul>
